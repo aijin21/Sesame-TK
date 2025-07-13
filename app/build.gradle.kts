008b704f97eb9c889071f3c51d1e89478ae8d4f9
aijin21 <107228198@qq.com>
2025-07-13 13:46:13 +0800

    ci(android): 更新 GitHub Actions 工作流和 APK 文件命名规则
    - 将时区设置步骤的名称改为中文
    - 更新各个构建步骤的名称为中文
    - 修改 APK 文件命名规则，使用 Sesame 代替 Sesame-TK，并使用版本号和构建标签
    - 优化部分步骤的描述，使其更加清晰

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -183,7 +183,7 @@ android {
         val variant = this
         variant.outputs.all {
             val flavorName = variant.flavorName.replaceFirstChar { it.uppercase() }
-            val fileName = "Sesame-TK-$flavorName-${variant.versionName}.apk"
+            val fileName = "Sesame-$flavorName-$$major.$minor.$patch-$buildTag.apk"
             (this as com.android.build.gradle.internal.api.BaseVariantOutputImpl).outputFileName = fileName
         }
     }
